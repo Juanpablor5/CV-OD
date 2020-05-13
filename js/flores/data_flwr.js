8a2de9feafe446aee4f362c5e4137474d36f69a6
Juan <jpr.arango10@uniandes.edu.co>
2020-05-13 10:31:49 -0500

    Flores terminadas

--- js/flores/data_flwr.js
+++ js/flores/data_flwr.js
@@ -3,75 +3,84 @@ $("#btn_select").click(function () {
     '<div id = "svgContent_si" ></div>';
   document.getElementById("flr_no").innerHTML =
     '<div id = "svgContent_no" ></div>';
-  document.getElementById("flr_asis").innerHTML =
-    '<div id = "svgContent_asis" ></div>';
   document.getElementById("flr_abs").innerHTML =
     '<div id = "svgContent_abs" ></div>';
+  document.getElementById("flr_asis").innerHTML =
+    '<div id = "svgContent_asis" ></div>';
   let selected = Array.from(etiquetas.selectedOptions).map(
     (option) => option.value
   );
-  let temp_eti_id = [];
-  let temp_proy_id = [];
+
+  let proy_etiq = [];
+
+  let flor_si = [];
+  let flor_no = [];
+  let flor_abs = [];
+  let flor_asis = [];
+  selected.forEach((seleccionada) => {
+    proy_etiq.push({ nombre: seleccionada, proyectos: [] });
+  });
+
   fetch("data/json/CV/Proyectos.json")
     .then((resp) => resp.json())
     .then((data) => {
       data.proyecto.forEach((proyecto) => {
         proyecto.etiquetas.forEach((etiqueta) => {
-          selected.forEach((seleccionada) => {
-            if (seleccionada == etiqueta.nombre) {
-              temp_eti_id.push(etiqueta.id);
-              temp_proy_id.push(proyecto.id);
+          proy_etiq.forEach((seleccionada) => {
+            if (seleccionada.nombre == etiqueta.nombre) {
+              seleccionada.proyectos.push({
+                id: proyecto.id,
+                votos: [0, 0, 0, 0],
+              });
             }
           });
         });
       });
 
-      var eti_id_arr = [];
-      var proy_id_arr = [];
-      $.each(temp_eti_id, function (i, el) {
-        if ($.inArray(el, eti_id_arr) === -1) eti_id_arr.push(el);
-      });
-      $.each(temp_proy_id, function (i, el) {
-        if ($.inArray(el, proy_id_arr) === -1) proy_id_arr.push(el);
-      });
-
-      console.log(eti_id_arr);
-      console.log(proy_id_arr);
-
-      let data_si_flwr = [];
-
-      let cont = [];
-
-      let cont_si = 0;
-      let cont_no = 0;
-      let cont_abs = 0;
-      let cont_asis = 0;
-
-      fetch("data/json/CV/Proyectos.json")
+      fetch(`data/json/CV/Votos_congresistas/Votos_Unido.json`)
         .then((resp) => resp.json())
-        .then((data_proy) => {
+        .then((data_vot) => {
+          proy_etiq.forEach((eti_selec) => {
+            let cont_si_tot = 0;
+            let cont_no_tot = 0;
+            let cont_abs_tot = 0;
+            let cont_asis_tot = 0;
+            data_vot.congresista.forEach((congresista) => {
+              congresista.proyecto.forEach((proyecto) => {
+                let cont_si = 0;
+                let cont_no = 0;
+                let cont_abs = 0;
+                let cont_asis = 0;
+                eti_selec.proyectos.forEach((proy_selec) => {
+                  if (proyecto.id === proy_selec.id) {
+                    cont_si += proyecto.voto.si;
+                    cont_no += proyecto.voto.no;
+                    cont_abs += proyecto.voto.se_abstuvo;
+                    cont_asis += proyecto.voto.no_asistio;
 
-          for (let c = -100; c < 600; i += 100) {
-            let k = c;
-            if (c + 200 == 700) c = 469;
-
-            fetch(`data/json/CV/Votos_congresistas/Votos_${k + 101}-${c + 200}.json.json`)
-              .then((resp) => resp.json())
-              .then((data_vot) => {
-                data_vot.congresista.forEach(proyecto => {
-                  proy_id_arr.forEach(id_proy => {
-                    proyecto.find(id_p => id_p.id == id_proy)
-                  });                  
+                    proy_selec.votos[0] += cont_si;
+                    proy_selec.votos[1] += cont_no;
+                    proy_selec.votos[2] += cont_abs;
+                    proy_selec.votos[3] += cont_asis;
+                  }
                 });
+                cont_si_tot += cont_si;
+                cont_no_tot += cont_no;
+                cont_abs_tot += cont_abs;
+                cont_asis_tot += cont_asis;
               });
-          }
+            });
+            flor_si.push({ Tema: eti_selec.nombre, abs: cont_si_tot });
+            flor_no.push({ Tema: eti_selec.nombre, abs: cont_no_tot  });
+            flor_abs.push({ Tema: eti_selec.nombre, abs: cont_abs_tot  });
+            flor_asis.push({Tema: eti_selec.nombre, abs: cont_asis_tot });
+          });
+        })
+        .then((_) => {
+          flower_si(flor_si);
+          flower_no(flor_no);
+          flower_abs(flor_abs);
+          flower_asis(flor_asis);
         });
-      flower_si(data_si_flwr);
-
-      // let data_si_flwr=[]
-      // for (let i = 0; i < selected.length; i++) {
-      //   data_si_flwr.push({Tema: selected[i], abs: (i + (i + 1) * 14)})
-      // }
-      // flower_si(data_si_flwr);
     });
 });
