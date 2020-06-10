09ca5e21947ce684f5a595274a02fc45dd6f6d73
Andrea <ac.beltrans@uniandes.edu.co>
2020-06-09 23:40:26 -0500

    Filtrado con solo categorías

--- js/scripts/OD/filtrarAños.js
+++ js/scripts/OD/filtrarAños.js
@@ -7,6 +7,8 @@ $("#btn_etiquetas, #btn_anios").click(function () {
   let otherObj = undefined;
   let newObj = undefined;
   let llego = false;
+  let preguntasPorEtiquetas = [];
+  let newObjE = undefined;
 
   let etiquetasSeleccionadas = Array.from(etiquetas.selectedOptions).map(
     (option) => option.value
@@ -21,59 +23,96 @@ $("#btn_etiquetas, #btn_anios").click(function () {
     fetch("data/json/OD/Preguntas.json")
       .then((resp) => resp.json())
       .then((data) => {
-        for (let j = 0; j < data.preguntas.length; j++) {
-          let pregunta = data.preguntas[j];
-          for (let i=0; i<aniosEscogidos.length; i++) {
-            let anioEscogido = aniosEscogidos[i];
-            if (anioEscogido == pregunta[anioEscogido]) {
-                otherObj = {año: anioEscogido, codigo: pregunta["codigo"], nombre: pregunta["pregunta"], etiqueta: pregunta["etiqueta"]};
-                preguntasAnio.push(otherObj);
-                break;
-            }
-          }
-        }
-        if (etiquetasSeleccionadas.length > 0) {
-          preguntasAnio.forEach(pregunta => {
-            let etiqueta = pregunta.etiqueta;
-            for (let i=0; i<etiquetasSeleccionadas.length; i++) {
-              let etiquetasSeleccionada = etiquetasSeleccionadas[i];
-              if (etiquetasSeleccionada === etiqueta) {
-                  newObj = {codigo: pregunta.codigo, nombre: pregunta.nombre, etiqueta: etiqueta};
-                  preguntasFinales.push(newObj);
+        if (aniosEscogidos.length > 0) {
+          for (let j = 0; j < data.preguntas.length; j++) {
+            let pregunta = data.preguntas[j];
+            for (let i=0; i<aniosEscogidos.length; i++) {
+              let anioEscogido = aniosEscogidos[i];
+              if (anioEscogido == pregunta[anioEscogido]) {
+                  otherObj = {año: anioEscogido, codigo: pregunta["codigo"], nombre: pregunta["pregunta"], etiqueta: pregunta["etiqueta"]};
+                  preguntasAnio.push(otherObj);
                   break;
               }
             }
+          }
+          if (etiquetasSeleccionadas.length > 0) {
+            preguntasAnio.forEach(pregunta => {
+              let etiqueta = pregunta.etiqueta;
+              for (let i=0; i<etiquetasSeleccionadas.length; i++) {
+                let etiquetasSeleccionada = etiquetasSeleccionadas[i];
+                if (etiquetasSeleccionada === etiqueta) {
+                    newObj = {codigo: pregunta.codigo, nombre: pregunta.nombre, etiqueta: etiqueta};
+                    preguntasFinales.push(newObj);
+                    break;
+                }
+              }
+            });
+            llego = true
+          }
+          if (preguntasFinales.length == 0 && !llego) {
+            preguntasAnio.forEach(pregunta => {
+              object.push({text: pregunta.nombre });
+            });
+          }
+          else {
+            document.getElementById("sec_preg").innerHTML = '<select id="preguntas2" multiple class="font"></select>>';
+            preguntasFinales.forEach(pregunta => {
+              object.push({text: pregunta.nombre });
+            });
+          }
+          let newP = document.createElement("p");
+          newP.setAttribute("id", "conteo_preguntas");
+          let newContent = document.createTextNode(object.length);
+          newP.appendChild(newContent);
+          let currentP = document.getElementById("conteo_preguntas");
+          currentP.parentNode.replaceChild(newP,currentP);
+          let select = new SlimSelect({
+            select: "#preguntas2",
+            placeholder: "Preguntas",
+            searchPlaceholder: "Buscar preguntas",
+            showSearch: true, // shows search field,
+            searchingText: "Buscando...",
+            searchText: "No se encontró la pregunta",
+            closeOnSelect: false,
+            data: object,
           });
-          llego = true
         }
-        if (preguntasFinales.length == 0 && !llego) {
-          preguntasAnio.forEach(pregunta => {
-            object.push({text: pregunta.nombre });
-          });
-        }
-        else {
+        else if (etiquetasSeleccionadas.length > 0 && aniosEscogidos.length == 0) {
+          for (let j = 0; j < data.preguntas.length; j++) {
+            let pregunta = data.preguntas[j];
+            for (let i=0; i<etiquetasSeleccionadas.length; i++) {
+              let etiquetasSeleccionada = etiquetasSeleccionadas[i];
+              if (etiquetasSeleccionada == pregunta["etiqueta"]) {
+                  newObjE = {nombre: pregunta["pregunta"]};
+                  preguntasPorEtiquetas.push(newObjE);
+                  break;
+              }
+            }
+          }
           document.getElementById("sec_preg").innerHTML = '<select id="preguntas2" multiple class="font"></select>>';
-          preguntasFinales.forEach(pregunta => {
-            object.push({text: pregunta.nombre });
+          preguntasPorEtiquetas.forEach(pregunta => {
+            object.push({ text: pregunta.nombre });
+          });
+          let newP = document.createElement("p");
+          newP.setAttribute("id", "conteo_preguntas");
+          let newContent = document.createTextNode(object.length);
+          newP.appendChild(newContent);
+          let currentP = document.getElementById("conteo_preguntas");
+          currentP.parentNode.replaceChild(newP,currentP);
+          // console.log(object)
+          let select = new SlimSelect({
+            select: "#preguntas2",
+            placeholder: "Preguntas",
+            searchPlaceholder: "Buscar preguntas",
+            showSearch: true, // shows search field,
+            searchingText: "Buscando...",
+            searchText: "No se encontró la pregunta",
+            closeOnSelect: false,
+            data: object,
           });
         }
-        let newP = document.createElement("p");
-        newP.setAttribute("id", "conteo_preguntas");
-        let newContent = document.createTextNode(object.length);
-        newP.appendChild(newContent);
-        let currentP = document.getElementById("conteo_preguntas");
-        currentP.parentNode.replaceChild(newP,currentP);
-        let select = new SlimSelect({
-          select: "#preguntas2",
-          placeholder: "Preguntas",
-          searchPlaceholder: "Buscar preguntas",
-          showSearch: true, // shows search field,
-          searchingText: "Buscando...",
-          searchText: "No se encontró la pregunta",
-          closeOnSelect: false,
-          data: object,
+
         });
-      });
   }, 300);
 });
 
